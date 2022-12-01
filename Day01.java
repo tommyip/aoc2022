d679b6d3f164a236d7d1b5b5d7c9a75aa15542b1
Thomas Ip <thomas@ipthomas.com>
2022-12-01 21:18:03 +0000

    Refactor Day 01

--- Day01.java
+++ Day01.java
@@ -1,24 +1,24 @@
-import java.nio.file.Files;
-import java.nio.file.Path;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Iterator;
-import java.util.Vector;
 
 public class Day01 {
     public static void main(String[] args) throws Exception {
-        Vector<Integer> calories = new Vector<>();
+        ArrayList<Integer> calories = new ArrayList<>();
         int runningCount = 0;
-        Iterator<String> lines = Files.lines(Path.of("inputs/day01.txt")).iterator();
-        while (lines.hasNext()) {
-            String line = lines.next().trim();
-            if (line.isEmpty()) {
-                calories.add(runningCount);
-                runningCount = 0;
-            } else {
-                runningCount += Integer.parseInt(line);
+        try (BufferedReader br = new BufferedReader(new FileReader("inputs/day01.txt"))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                if (line.isEmpty()) {
+                    calories.add(runningCount);
+                    runningCount = 0;
+                } else {
+                    runningCount += Integer.parseInt(line);
+                }
             }
         }
-        Collections.sort(calories, Collections.reverseOrder());
+        calories.sort(Collections.reverseOrder());
         System.out.println(calories.get(0));
         System.out.println(calories.subList(0, 3).stream().reduce(0, (a, b) -> a + b));
     }
