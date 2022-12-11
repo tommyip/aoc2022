2cfa30ec5e182c28757d97d24bdc333bdf184b2b
Thomas Ip <thomas@ipthomas.com>
2022-12-11 13:58:17 +0000

    Day 11 part 1

---
+++ Day11.py
@@ -0,0 +1,35 @@
+from collections import namedtuple
+import operator
+
+Monkey = namedtuple('Monkey', ['items', 'op', 'op_value', 'divisor', 'throw_true', 'throw_false'])
+
+with open('inputs/day11.txt') as f:
+    blocks = f.read().strip().split('\n\n')
+    monkeys = []
+    for block in blocks:
+        lines = block.split('\n')
+        items_list = lines[1].split(': ')
+        starting_items = []
+        if len(items_list) == 2:
+            starting_items = [int(item) for item in items_list[1].split(', ')]
+        [op, value] = lines[2].split(' ')[-2:]
+        op = operator.add if op == '+' else operator.mul
+        value = None if value == 'old' else int(value)
+        divisor = int(lines[3].split()[-1])
+        throw_true = int(lines[4].split()[-1])
+        throw_false = int(lines[5].split()[-1])
+        monkeys.append(Monkey(starting_items, op, value, divisor, throw_true, throw_false))
+
+inspections = [0] * len(monkeys)
+
+for round in range(20):
+    for i, monkey in enumerate(monkeys):
+        for item in monkey.items:
+            item = monkey.op(item, monkey.op_value or item)
+            item //= 3
+            monkeys[monkey.throw_true if item % monkey.divisor == 0 else monkey.throw_false].items.append(item)
+        inspections[i] += len(monkey.items)
+        monkey.items.clear()
+
+inspections.sort()
+print(inspections[-1] * inspections[-2])
