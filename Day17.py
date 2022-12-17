0f27b22a7ab8d85bbb63dc71b57953b4c112ef3b
Thomas Ip <thomas@ipthomas.com>
2022-12-17 21:27:44 +0000

    Day 17 part 2

--- Day17.py
+++ Day17.py
@@ -1,5 +1,3 @@
-from tqdm import tqdm
-
 class Rock:
     def __init__(self, parts, pos):
         self.parts = parts
@@ -21,48 +19,64 @@ rock3 = [0+0j, 0+1j, 0+2j, 0+3j]
 rock4 = [0+0j, 1+0j, 0+1j, 1+1j]
 rocks = [rock0, rock1, rock2, rock3, rock4]
 
-m = set()
-jet_i = 0
-rock_i = 0
-level = 0
+def height(n):
+    m = set()
+    jet_i = 0
+    rock_i = 0
+    level = 0
 
-def collide(rock):
-    for part in rock.parts:
-        if part.imag == -1:  # on the floor
-            return True
-        if part in m:  # on a rock
-            return True
-        if part.real == -1 or part.real == 7: # walls
-            return True
-    return False
-
-
-def fall(rock):
-    global jet_i
-    while True:
-        rock.move(jet[jet_i])
-        if collide(rock):
-            rock.move(-jet[jet_i])
-        jet_i = (jet_i + 1) % len(jet)
-        rock.move(-1j)
-        if collide(rock):
-            rock.move(1j)
-            break
-
-def visualize(levels):
-    for j in reversed(levels):
-        print(''.join('#' if complex(i, j) in m else '.' for i in range(0, 7)))
+    def collide(rock):
+        for part in rock.parts:
+            if part.imag == -1:  # on the floor
+                return True
+            if part in m:  # on a rock
+                return True
+            if part.real == -1 or part.real == 7: # walls
+                return True
+        return False
 
 
-for i in tqdm(range(1000)):
-    rock = Rock(list(rocks[rock_i]), complex(2, level + 3))
-    fall(rock)
-    m.update(set(rock.parts))
-    level = max(level, max(part.imag for part in rock.parts) + 1)
-    rock_i = (rock_i + 1) % len(rocks)
-    if i == 2021:
-        print(int(level))
+    def fall(rock):
+        nonlocal jet_i
+        while True:
+            rock.move(jet[jet_i])
+            if collide(rock):
+                rock.move(-jet[jet_i])
+            jet_i = (jet_i + 1) % len(jet)
+            rock.move(-1j)
+            if collide(rock):
+                rock.move(1j)
+                break
 
-visualize(range(1000))
+    def visualize(levels):
+        for j in reversed(levels):
+            print(''.join('#' if complex(i, j) in m else '.' for i in range(0, 7)))
 
-print(int(level))
+    patterns = set()
+    last = 0
+    prev_level = 0
+
+    for i in range(n):
+        pattern = (rock_i, jet_i)
+        # if pattern in patterns:
+        #     print(i, pattern)
+        # if pattern == (0, 489):
+        #     print(i, i - last, level - prev_level)
+        #     last = i
+        #     prev_level = level
+        patterns.add(pattern)
+        rock = Rock(list(rocks[rock_i]), complex(2, level + 3))
+        fall(rock)
+        m.update(set(rock.parts))
+        level = max(level, int(max(part.imag for part in rock.parts) + 1))
+        rock_i = (rock_i + 1) % len(rocks)
+
+    return level
+
+print(height(2022))
+
+head = height(80)
+n_repeat = (1000000000000 - 80) // 1740
+repeat_height = 2759
+tail = height(1100 + 80)-height(80)
+print(head + n_repeat * repeat_height + tail)
